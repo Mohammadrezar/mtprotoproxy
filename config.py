77e078da68b975c1477b2be2a3fd654b7a154e82
Mohammad <mrr1377525@gmail.com>
2019-04-23 00:53:39 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
-PORT = 3256
-
-# name -> secret (32 hex chars)
+# Channel : @TeleDiamondCh == @Mrr619
+# عدد پورت دلخواه
+PORT = 219
+# شما میتونید سکرت دلخواه خود را بجای دو سکرت پایین قرار بدین
+# سکرت شما باید 32 حرف باشد
 USERS = {
     "tg":  "00000000000000000000000000000000",
-    "tg2": "0123456789abcdef0123456789abcdef"
+    "tg2": "0123456789TeleDiamondCh123456789"
 }
+
+# Makes the proxy harder to detect
+# Can be incompatible with very old clients
+SECURE_ONLY = True
 
 # Tag for advertising, obtainable from @MTProxybot
 # AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
